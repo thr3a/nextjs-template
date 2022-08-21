561659b1bc83fe177f193787eaf8a6a4cfc7168c
thr3a <thr3a@example.com>
2022-08-21 12:15:54 +0900

    add: mantine導入

---
+++ pages/page2.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Container } from '@mantine/core';
+
+const Page2: NextPage = () => {
+  return (
+    <Container>
+      <h1>page2</h1>
+    </Container>
+  )
+}
+
+export default Page2
