5e3c8734cf942e99da12caa2a932e2ebc3b2a889
thr3a <thr3a@example.com>
2022-08-21 12:22:19 +0900

    add: アイコン追加

--- pages/page2.tsx
+++ pages/page2.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
 import { Container } from '@mantine/core';
+import { IconDownload } from '@tabler/icons';
 
 const Page2: NextPage = () => {
   return (
     <Container>
       <h1>page2</h1>
+      <IconDownload></IconDownload>
     </Container>
   );
 };
