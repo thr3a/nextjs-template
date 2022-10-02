0998a929b125b71503f2185cf777c9658ba6d2c6
thr3a <thr3a@example.com>
2022-10-03 03:54:20 +0900

    add:

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // basePath: process.env.GITHUB_ACTIONS && 'nextjs-template',
+  // trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
