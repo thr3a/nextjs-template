596f9396b0d02b99d0fe00141cf94cae37022082
thr3a <thr3a@example.com>
2023-03-28 01:18:43 +0900

    update: 使ってない実装削除

--- src/pages/page2.tsx
+++ src/pages/page2.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { IconDownload } from '@tabler/icons-react';
-import styles from '../styles/Home.module.scss';
 import { Button } from '@mantine/core';
 import Link from 'next/link';
 import { UserForm } from '@/features/form2/Form';
@@ -12,7 +11,6 @@ const Page2: NextPage = () => {
       <Head>
         <title>Page 2</title>
       </Head>
-      <h1 className={styles.title}>Page 2</h1>
       <IconDownload></IconDownload>
       <Button component={Link} href="https://www.google.com/">Google</Button>
       <UserForm></UserForm>
