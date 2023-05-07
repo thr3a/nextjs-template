5e555619f76d8a740abbb72ee7282a8fbf4cf298
thr3a <thr3a@example.com>
2023-05-07 15:17:52 +0900

    add: APIにzod導入

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// POSTの場合はreq.queryをreq.bodyにすること
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { z } from 'zod';
 
 type Data = {
   name: string;
@@ -17,14 +19,22 @@ export type ErrorResponseProps = {
 
 export type ResponseProps = SuccessResponseProps | ErrorResponseProps;
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseProps>) {
+const requestSchema = z.object({
+  name: z.string(),
+  error: z.boolean().optional(),
+});
+
+type RequestProps = NextApiRequest & {
+  query: z.infer<typeof requestSchema>;
+}
+
+export default function handler(req: RequestProps, res: NextApiResponse<ResponseProps>) {
   // if (req.method !== 'POST') {
   //   res.status(405).send({status: 'ng', errorMessage: 'Method Not Allowed'});
   //   res.end();
   //   return;
   // }
-
-  if (req.query.error === '1') {
+  if (req.query.error) {
     res.status(500).json({
       status: 'ng',
       errorMessage: 'Internal Server Error'
@@ -32,7 +42,14 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Respon
     res.end();
     return;
   }
+  const reqData = requestSchema.safeParse(req.query);
+  if (!reqData.success) {
+    console.log(reqData.error);
+    res.status(400).send({status: 'ng', errorMessage: 'Invalid request'});
+    res.end();
+    return;
+  }
 
-  res.status(200).json({status: 'ok', result: {name: 'John Smith'}});
+  res.status(200).json({status: 'ok', result: {name: reqData.data.name}});
   res.end();
 }
