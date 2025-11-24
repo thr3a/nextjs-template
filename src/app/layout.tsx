71110b380f93d74ac16326d86b6616e617a08725
thr3a <thr3a@example.com>
2025-11-24 12:01:43 +0900

    update: nextjs16 mantine8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 // import React from 'react';
+import '@mantine/core/styles.css';
+
 import { Providers } from '@/providers';
 import { theme } from '@/theme';
 import { ColorSchemeScript, Container, MantineProvider, Title, mantineHtmlProps } from '@mantine/core';
 
-import '@mantine/core/styles.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
