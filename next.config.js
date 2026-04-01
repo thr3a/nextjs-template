fa4772af78d794e447ed0805adbab88f5a374cc1
thr3a <thr3a@example.com>
2026-04-02 05:30:55 +0900

    fix: localhost以外でも開発できるように

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // basePath: process.env.GITHUB_ACTIONS && 'nextjs-template',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
   poweredByHeader: false,
-  // github pagesの場合
-  // output: 'export',
-  // k8sの場合
-  output: 'standalone'
+  output: 'standalone',
+  allowedDevOrigins: ['192.168.16.12', 'ubuntu02.local']
 };
 
 module.exports = nextConfig;
